9b3ddd74afc06529135c8d1babc3957700c68c95
Gabriela Mota <brilacerda@users.noreply.github.com>
2016-11-28 00:22:39 -0300

    bugs solved

--- render.py
+++ render.py
@@ -2,7 +2,8 @@ from PIL import Image
 import random
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import numpy as np
+import numpy
+from math import sqrt
 
 fitness = []
 
@@ -13,6 +14,7 @@ def main():
 
 	centroid_colors, centroid_array_positions = generatePopulation(image, clusters)
 
+	image = fitness_n_color_distance(image, centroid_colors)
 
 	imgplot = plt.imshow(image)
 	plt.show()
@@ -39,10 +41,13 @@ def generatePopulation(image, clusters):
 	return population, centroids
 
 # Calculates euclidean distance between a data point and all the available cluster centroids.      
-def euclidean_dist(image, centroids):
+def fitness_n_color_distance(image, centroids):
+	global fitness
 	cluster_debut = []
 	image_classification = []
+	quantized_image = []
 
+	image = numpy.reshape(image, (-1, 3))
 	# Check if all clusters have at least one pixel associated
 	for i in centroids:
 		cluster_debut.append(False)
@@ -60,27 +65,36 @@ def euclidean_dist(image, centroids):
 		for centroid in centroids:
 			r1, g1, b1 = pixel
 			r2, g2, b2 = centroid
+			#print r1, g1, b1, '--', r2, g2, b2
 
 			# code reuse
-			r = r2-r1
-			g = g2-g1
-			b = b2-b1
+			(int r) = r2-r1
+			(int g) = g2-g1
+			(int b) = b2-b1
 
-			color_distance = sqrt(r^2+ g^2+ b^2)
+			fitness = r^2+ g^2+ b^2
+			'''
+				Calculate the fitness and the color distance
+			'''
+
+			color_distance = sqrt(fitness)
 
 			if color_distance < smaller_distance[0]:
-		 		smaller_distance[0] = color_distance
-		 		smaller_distance[1] = centroid_array_position
+		 		smaller_distance= (fitness, centroid_array_position)
 
 		 	centroid_array_position = centroid_array_position +1
 
 		 # Saves all the distances and the 
 		image_classification.append(smaller_distance)
+		# the quantized image matrix takes the color of the centroid with the smaller
+		# distance between the pixel and the centroid
+		quantized_image.append(centroids[smaller_distance[1]])
 
 		if not cluster_debut[smaller_distance[1]]:
 			cluster_debut[smaller_distance[1]] = True
 
 	image_classification = numpy.reshape(image_classification, (len(image), -1))
+	quantized_image = numpy.reshape(quantized_image, (len(image), -1))
 
     # If any cluster is empty then assign one point from data set randomly so as to not have empty
     # clusters and 0 means.        
@@ -93,18 +107,8 @@ def euclidean_dist(image, centroids):
 			color_distance = sqrt((r2-r1)^2+(g2-g1)^2+(b2-b1)^2)
 			image_classification[x][y]
 		array_position = array_position +1
-	
-	'''
-		Calculate the fitness
-	'''
-	
-	global fitness
-	for i in range(len(image_classification)):
-		if image_classification[i][1] == 
-		
 
-
-    #return clusters
+	return quantized_image
 
 
 if __name__ == "__main__":
