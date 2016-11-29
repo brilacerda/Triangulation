73913ef4cbe04212de3aaf5fe628c75c3f969e27
Gabriela Mota <brilacerda@users.noreply.github.com>
2016-11-29 05:34:36 -0300

    Update pixels.py

--- pixels.py
+++ pixels.py
@@ -9,9 +9,14 @@ numpy.seterr(over='ignore')
 fitness = []
 
 def main():
+	global fitness
 	image = getPixelsList()
 	img_clone  = []
 	clusters = 2
+
+	# Initializing the fitness
+	for i in range(clusters):
+		fitness.append(0)
 
 	centroid_colors, centroid_array_positions = generatePopulation(image, clusters)
 	img_clone = fitness_n_color_distance(image, centroid_colors, len(image))
@@ -21,10 +26,10 @@ def main():
 	for i in range(30):
 		print i + 1, " iteration"
 
-		for j in range(int (floor(clusters*0.4))):
+		for j in range(int (floor(clusters*0.4))+1):
 			print "# Recombination ", i
 			if random.random() <= 0.9:
-				centroid_colors = recombine(img_clone.tolist(), clusters, centroid_colors)
+				centroid_colors = recombine(img_clone, clusters, centroid_colors)
 		if random.random() <= 0.3:
 			print "# Mutation"
 			centroid_colors = mutate(img_clone, clusters, centroid_colors)
@@ -63,9 +68,9 @@ def recombine(image, k, centroid_colors):
 	c, d = getPixelRandomly(image)
 	
 	r1g1b1 = image[a][b]
-	r1, g1, b1 = r1g1b1.tolist()
+	r1, g1, b1 = r1g1b1
 	r2g2b2 = image[c][d]
-	r2, g2, b2 = r2g2b2.tolist()
+	r2, g2, b2 = r2g2b2
 	child = random.randrange(k)
 	centroid_colors[child] = ((r2-r1)/2, (g2-g1)/2, (b2-b1)/2)
 	return centroid_colors
@@ -96,9 +101,14 @@ def formatToImage(image, column, line):
 
 	return img2
 
+def calculate_fitness(fitness_data):
+	global fitness
+
+	fitness[fitness_data[1]] = fitness[fitness_data[1]] + fitness_data[0]
+
+
 # Calculates euclidean distance between a data point and all the available cluster centroids.      
 def fitness_n_color_distance(image, centroids, width):
-	global fitness
 	cluster_debut = []
 	image_classification = []
 	quantized_image = []
@@ -128,17 +138,18 @@ def fitness_n_color_distance(image, centroids, width):
 				g = g2-g1
 				b = b2-b1
 
-				fitness = int (r)^2+ int (g)^2+ int (b)^2
-				
+				color_distance = abs(int (r)^2+ int (g)^2+ int (b)^2)
+				color_distance = sqrt(color_distance)
 				'''
 					Calculate the fitness and the color distance
 				'''
-				color_distance = sqrt(fitness)
 
 				if color_distance < smaller_distance[0]:
-			 		smaller_distance = (fitness, centroid_array_position)
+			 		smaller_distance = (int (r)+ int (g)+ int (b), centroid_array_position)
 
 			 	centroid_array_position = centroid_array_position +1
+
+			calculate_fitness(smaller_distance)
 
 			if not cluster_debut[smaller_distance[1]]:
 				cluster_debut[smaller_distance[1]] = True
@@ -152,16 +163,16 @@ def fitness_n_color_distance(image, centroids, width):
 
 		image_classification.append(aux_img_class)
 		quantized_image.append(aux_quatiz_img)
-		
+
     # If any cluster is empty then assign one point from data set randomly so as to not have empty
     # clusters and 0 means.        
 	array_position = 0
 	for debut in cluster_debut:
 		if not debut:
-			x, y = getPixelRandomly()
+			x, y = getPixelRandomly(image)
 			r1, g1, b1 = image[x][y]
 			r2, g2, b2 = centroids[array_position]
-			color_distance = sqrt((r2-r1)^2+(g2-g1)^2+(b2-b1)^2)
+			color_distance = sqrt(abs((r2-r1)^2+(g2-g1)^2+(b2-b1)^2))
 			image_classification[x][y]
 		array_position = array_position +1
 
